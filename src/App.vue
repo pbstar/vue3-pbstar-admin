235f679d80196f7e17d33dcddd9a6163bf83f4b3
pengbo <pbstar@sina.cn>
2024-06-17 10:48:34 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,8 @@
   <RouterView />
 </template>
 <script setup>
-import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
 import config from "@/config";
-onMounted(() => {
-  document.getElementById("p_app_loading_mask").style.display = "none";
-  console.log("Version："+config.version);
-})
+console.log("Version：" + config.version);
 </script>
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
